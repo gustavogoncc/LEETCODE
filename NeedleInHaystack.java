b011d42785fb100c816f9700d10cce6647428fdd
José Gustavo G. da Silva <105744484+gustavogoncc@users.noreply.github.com>
2023-07-04 23:30:49 -0300

    Create NeedleInHaystack.java

---
+++ NeedleInHaystack.java
@@ -0,0 +1,33 @@
+public class NeedleInHaystack {
+    public static int strStr(String haystack, String needle) {
+        if (needle.isEmpty()) {
+            return 0;
+        }
+        
+        int n = haystack.length();
+        int m = needle.length();
+        
+        for (int i = 0; i <= n - m; i++) {
+            int j;
+            for (j = 0; j < m; j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    break;
+                }
+            }
+            
+            if (j == m) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String haystack = "sadbutsad";
+        String needle = "sad";
+        
+        int index = strStr(haystack, needle);
+        System.out.println("Índice da primeira ocorrência: " + index);
+    }
+}
